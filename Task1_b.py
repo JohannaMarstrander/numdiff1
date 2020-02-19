6e5f7b819b856c8470c4f9bddbb79bcf44159a98
nannaberre <nanna_berre@hotmail.com>
2020-02-19 18:20:02 +0100

    Cleaning up files

--- Task1_b.py
+++ Task1_b.py
@@ -2,48 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Task1_a import BVP, solve_bvp, plott
 
+
 def u(x, y):
-    return np.sin(np.pi * 1/2 * x) + np.cos(np.pi  * y)
+    return np.sin(np.pi * 1 / 2 * x) + np.cos(np.pi * y)
 
 
 def f1(x, y):
-    return (((1/4 * np.pi ** 2) * np.sin(np.pi * 1/2 * x) + (np.pi) ** 2 * np.cos(np.pi * y))
-            + v(x, y)[0] * (1/2 * np.pi) * np.cos(np.pi * 1/2 * x) - v(x, y)[1] * ( np.pi) * np.sin(np.pi * y))
+    return (((1 / 4 * np.pi ** 2) * np.sin(np.pi * 1 / 2 * x) + (np.pi) ** 2 * np.cos(np.pi * y))
+            + v(x, y)[0] * (1 / 2 * np.pi) * np.cos(np.pi * 1 / 2 * x) - v(x, y)[1] * (np.pi) * np.sin(np.pi * y))
 
 
 def v(x, y):
     return np.array([x, y])
 
 
-def u2(x, y):
-    return np.exp(x) + 2 * np.exp(y)
-
-
-def f2(x, y):
-    return -0.5 * (np.exp(x) + 2 * np.exp(y)) + v2(x, y)[0] * np.exp(x) + 2 * v2(x, y)[1] * np.exp(
-        y)  # K=1/& * (np.exp(1) + 1*np.exp(1)/my + 1*2*np.exp(1)/my
-
-
-def v2(x, y):
-    return np.array([2 * x, y])
-
-mu = 1
-mu2 = 0.5
 # error analysis
-
-#error analysis
-M_list=[10, 20, 39, 76] #150
-E=[]
-h_list=[]
+M_list = [10, 20, 39, 76]  # 150
+E = []
+h_list = []
 
 for M1 in M_list:
     x, y = np.ogrid[0:1:(M1 + 1) * 1j, 0:1:(M1 + 1) * 1j]
-    ex = BVP(f1, v, u, 0, 1, mu, u)
+    ex = BVP(f1, v, u, 0, 1, 1, u)
     U = solve_bvp(ex, M1)
     U_ext = ex.uexact(x, y)
     if M1 == 150:
         plott(x, y, U.reshape((M1 + 1, M1 + 1)))
-        plott(x,y, U_ext)
+        plott(x, y, U_ext)
     U_ext = U_ext.ravel()
     error = U_ext - U
     E.append(np.linalg.norm(error, ord=np.inf))
@@ -56,6 +41,7 @@ h_list = np.array(h_list)
 
 plt.figure()
 plt.loglog(h_list, E, 'o-')
-#plt.loglog(h_list, h_list ** 2 * 1 / 12 * (np.exp(1) + 2 * 1 * np.exp(1) + 1 * 2 * np.exp(1)))
-plt.loglog(h_list, h_list ** 2 * 1 / 12 * ((1+1/16) * np.pi **4 + 18 * np.pi ** 3))
+print((1 / 12) * ((1) * np.pi ** 4 + (1 + 1 / 8) * np.pi ** 3))
+plt.loglog(h_list, h_list ** 2 * 1 / 12 * ((1 + 1 / 16) * np.pi ** 4 + 18 * np.pi ** 3))
+plt.loglog(h_list, h_list ** 2 * 1 / 12 * (np.pi ** 4 + (1 + 1 / 8) * np.pi ** 3), color='r')
 plt.show()
